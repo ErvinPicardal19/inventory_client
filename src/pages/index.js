ae7ae302d37a5fc7c33669ea321274fd7600e8c4
Ervin Picardal <ervinjohnpicardal@gmail.com>
2022-12-19 03:07:55 +0800

    Deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 export { default as Dashboard } from './Dashboard';
-export { default as Kanban } from './Kanban';
 export { default as Orders } from './Orders';
 export { default as Employees } from './Employees';
 export { default as Editor } from './Editor';
