19dd2d7fde0a8876380058f1da288b09846caa84
Ervin Picardal <ervinjohnpicardal@gmail.com>
2022-12-19 00:55:58 +0800

    change url

--- src/common/api/axios.js
+++ src/common/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://malasakit-api.onrender.com/api/v1'
+const BASE_URL = 'https://activecycletire-api.onrender.com/api/v1'
 
 export default axios.create({
    baseURL: BASE_URL
--- src/common/api/socket.js
+++ src/common/api/socket.js
@@ -1,5 +1,5 @@
 import {io} from 'socket.io-client'
 
-const DOMAIN_NAME = 'https://malasakit-api.onrender.com'
+const DOMAIN_NAME = 'https://activecycletire-api.onrender.com'
 
 export const socket = io.connect(DOMAIN_NAME)
